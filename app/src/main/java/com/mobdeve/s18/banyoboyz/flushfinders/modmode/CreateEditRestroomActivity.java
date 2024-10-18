918d76fd4cabe0560656434f4a7dbad97f792063
Amatakaze <AaronDichoso17@gmail.com>
2024-10-18 22:58:09 +0800

    Refactored Layout Files to have appropriate IDs

--- app/src/main/java/com/mobdeve/s18/banyoboyz/flushfinders/modmode/CreateEditRestroomActivity.java
+++ app/src/main/java/com/mobdeve/s18/banyoboyz/flushfinders/modmode/CreateEditRestroomActivity.java
@@ -16,7 +16,7 @@ public class CreateEditRestroomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_create_edit_restroom);
+        setContentView(R.layout.activity_get_restroom_data);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
