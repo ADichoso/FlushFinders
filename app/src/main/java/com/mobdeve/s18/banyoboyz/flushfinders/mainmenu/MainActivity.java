80a0fe1e0aab3932f7e6804200ed43733c5b7f32
Amatakaze <AaronDichoso17@gmail.com>
2024-11-04 00:02:14 +0800

    Opening the App at the start when you just logged in automatically redirects you to your respective account home page.

--- app/src/main/java/com/mobdeve/s18/banyoboyz/flushfinders/mainmenu/MainActivity.java
+++ app/src/main/java/com/mobdeve/s18/banyoboyz/flushfinders/mainmenu/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mobdeve.s18.banyoboyz.flushfinders.mainmenu;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,10 +12,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.mobdeve.s18.banyoboyz.flushfinders.adminmode.AdminHomeActivity;
+import com.mobdeve.s18.banyoboyz.flushfinders.models.AccountData;
+import com.mobdeve.s18.banyoboyz.flushfinders.models.SharedPrefReferences;
+import com.mobdeve.s18.banyoboyz.flushfinders.modmode.ModHomeActivity;
 import com.mobdeve.s18.banyoboyz.flushfinders.usermode.MapHomeActivity;
 import com.mobdeve.s18.banyoboyz.flushfinders.R;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedpreferences;
+    String account_name;
+    String account_email;
+    String account_type;
+    int account_pp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +38,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // getting the data which is stored in shared preferences.
+        sharedpreferences = getSharedPreferences(SharedPrefReferences.SHARED_PREFS, Context.MODE_PRIVATE);
+
+        // Check if user is already logged in
+        account_name = sharedpreferences.getString(SharedPrefReferences.ACCOUNT_NAME_KEY, "");
+        account_email = sharedpreferences.getString(SharedPrefReferences.ACCOUNT_EMAIL_KEY, "");
+        account_type = sharedpreferences.getString(SharedPrefReferences.ACCOUNT_TYPE_KEY, "");
+        account_pp = sharedpreferences.getInt(SharedPrefReferences.ACCOUNT_PP_KEY, -1);
+
+        // check if the fields are not null then one current user is logged in
+        if (areFieldsNotEmpty(new String[]{account_name, account_email, account_type}) && account_pp != -1)
+        {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+            startActivity(intent);
+        }
+    }
+
+    private boolean areFieldsNotEmpty(String[] fields)
+    {
+        for(String field : fields)
+            if (!field.isEmpty())
+                return true;
+
+        return false;
     }
 
     public void useGuestButton(View view)
